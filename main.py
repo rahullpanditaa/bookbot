2e2991b0657d2382e016a0ad6dac7fc67fadd781
Rahul Pandita <rpandita1999@outlook.com>
2025-06-16 08:53:16 +0000

    create function to count every character in text; function to return sorted list of characters

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from stats import get_number_of_words
+from stats import get_number_of_words, count_every_character, sort_dict_of_characters
 
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
-    print(f"{get_number_of_words(frankenstein)} words found in the document")
+    character_count = count_every_character(frankenstein)
+    sorted_characters = sort_dict_of_characters(character_count)
 
+    generate_report("books/frankenstein.txt", get_number_of_words(frankenstein), sorted_characters)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         book_contents = f.read()    
     return book_contents
 
+
+def generate_report(filepath, word_count, char_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for ch in char_list:
+        print(ch["character"], ch["num"], sep=": ")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,20 @@
 def get_number_of_words(text):
     return len(text.split())
+
+def count_every_character(text):
+    num_of_characters = {}
+    for ch in text.lower():
+        if ch not in num_of_characters:
+            num_of_characters[ch] = 1
+        else:
+            num_of_characters[ch] += 1
+    return num_of_characters
+
+def sort_dict_of_characters(dict_of_characters):
+    sorted_list_of_characters = []
+
+    for ch in dict_of_characters:
+        if ch.isalpha():
+            sorted_list_of_characters.append({"character" : ch, "num" : dict_of_characters[ch]})
+        
+    return sorted(sorted_list_of_characters, key=lambda char: char["num"], reverse=True)
